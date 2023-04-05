bee0eace4cc31871b68e98c60c62554868cbf810
Blackcastus <blackcastus@gmail.com>
2023-04-05 10:54:05 +0700

    delete file Esp32_freeRTOS

--- Esp32_Freertos/main/Linear_Regression/linear_regression.h
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include "freertos/FreeRTOS.h"
-#include "freertos/task.h"
-
-void Linear_Regression(void *arg);
-void Linear_Regression_calc(float *a, float *b, float *arry_x, float *arry_y, int point);
